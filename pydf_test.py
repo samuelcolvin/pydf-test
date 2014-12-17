33003ac42820ee9f4c58572fc1cd0972889fc1c2
Samuel Colvin <s@muelcolvin.com>
2014-12-17 17:19:27 +0000

    add version info, update requirements

--- pydf_test.py
+++ pydf_test.py
@@ -6,7 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<div>go to <a href="/pdf">pdf</a> to view a PDF.</div>'
+    return '<div>go to <a href="/pdf">pdf</a> to view a PDF, ' \
+           'or to <a href="/version">version</a> to see version info.</div>'
+
+
+@app.route('/version')
+def version():
+    from pydf import get_version
+    info = 'VERSION INFO:\n%s' % get_version()
+    return Response(info, content_type='text/plain')
 
 
 @app.route('/pdf')
@@ -18,10 +26,10 @@ def pdf():
         response = make_response(pdf_content)
         response.headers['Content-Type'] = 'application/pdf'
         response.headers['Content-Disposition'] = 'inline; filename=test.pdf'
+        return response
     except Exception:
         exc = traceback.format_exc()
-        response = Response(exc, content_type='text/plain')
-    return response
+        return Response(exc, content_type='text/plain')
 
 if __name__ == "__main__":
     app.run(debug=True)
